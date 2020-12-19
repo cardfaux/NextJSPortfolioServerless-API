328037bfe2773145f1bf7ab7fbd27820cf96d117
cardfaux <cardfaux@tutanota.com>
2020-12-19 11:41:42 -0600

    added values to .env from auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,15 +6,27 @@ const config = require('../config/dev');
 // This middleware will check access token in authorization headers
 // of a request
 // It will verify access token against Auth0 JSON web key set
+// exports.checkJwt = jwt({
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 10,
+//     jwksUri: 'https://dev-pxt8cafa.eu.auth0.com/.well-known/jwks.json'
+//   }),
+//   audience: 'https://dev-pxt8cafa.eu.auth0.com/api/v2/',
+//   issuer: 'https://dev-pxt8cafa.eu.auth0.com/',
+//   algorithms: ['RS256']
+// });
+
 exports.checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 10,
-    jwksUri: 'https://dev-pxt8cafa.eu.auth0.com/.well-known/jwks.json'
+    jwksUri: process.env.JWKSURI
   }),
-  audience: 'https://dev-pxt8cafa.eu.auth0.com/api/v2/',
-  issuer: 'https://dev-pxt8cafa.eu.auth0.com/',
+  audience: process.env.AUDIENCE,
+  issuer: process.env.ISSUER,
   algorithms: ['RS256']
 });
 
