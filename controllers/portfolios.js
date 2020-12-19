7100d0f1e8880149f771abae914ad96fb218a12e
cardfaux <cardfaux@tutanota.com>
2020-12-19 13:54:32 -0600

    added a sort function to the getPortfolios controller

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const Portfolio = mongoose.model('Portfolio');
 
 exports.getPortfolios = async (req, res) => {
-  const portfolios = await Portfolio.find({});
+  const portfolios = await Portfolio.find({}).sort({ createdAt: -1 });
   return res.json(portfolios);
-}
+};
 
 exports.getPortfolioById = async (req, res) => {
   try {
     const portfolio = await Portfolio.findById(req.params.id);
     return res.json(portfolio);
-  } catch(error) {
+  } catch (error) {
     return res.status(422).send(error.message);
   }
-}
+};
 
 exports.createPortfolio = async (req, res) => {
   const portfolioData = req.body;
@@ -24,23 +24,30 @@ exports.createPortfolio = async (req, res) => {
   try {
     const newPortfolio = await portfolio.save();
     return res.json(newPortfolio);
-  } catch(error) {
+  } catch (error) {
     return res.status(422).send(error.message);
   }
-}
+};
 
 exports.updatePortfolio = async (req, res) => {
-  const { body, params: {id}} = req;
+  const {
+    body,
+    params: { id },
+  } = req;
 
   try {
-    const updatedPortfolio = await Portfolio.findOneAndUpdate({_id: id}, body, {new: true, runValidators: true})
+    const updatedPortfolio = await Portfolio.findOneAndUpdate(
+      { _id: id },
+      body,
+      { new: true, runValidators: true }
+    );
     return res.json(updatedPortfolio);
-  } catch(error) {
+  } catch (error) {
     return res.status(422).send(error.message);
   }
-}
+};
 
 exports.deletePortfolio = async (req, res) => {
-  const portfolio = await Portfolio.findOneAndRemove({_id: req.params.id});
-  return res.json({_id: portfolio.id})
-}
+  const portfolio = await Portfolio.findOneAndRemove({ _id: req.params.id });
+  return res.json({ _id: portfolio.id });
+};
